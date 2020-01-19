79d300669013ef1d8c283983864ba9568ac4e09d
梁瑞豪 <18733558325@163.com>
2020-01-19 23:20:11 +0800

    '底部添加状态栏'

--- src/main/java/frame/TabCard.java
+++ src/main/java/frame/TabCard.java
@@ -18,20 +18,56 @@ public class TabCard extends JPanel {
     private JTextArea jTextArea;
     // 创建滚动面板, 指定滚动显示的视图组件(textArea), 垂直滚动条一直显示, 水平滚动条一直显示
     private  JScrollPane jScrollPane;
+    //创建工具栏组件
+    private JToolBar jToolBar;
+    //文件状态
+    private JLabel fileState;
+    //文件属性
+    private JLabel fileAttribute;
+
     //文件绝对路径
     private String absoluteFilePath;
     //文件名称（短名称）
     private String fileName;
     public TabCard() {
         super();
+        createObject();
+    }
+    /**
+     * 创建对象
+     * @Author lrh
+     * @Date 2020/1/19 23:05
+     * @Param []
+     * @Return void
+     */
+    private void createObject(){
         jTextArea = new JTextArea(20,20);
         jScrollPane = new JScrollPane(jTextArea);
+        jToolBar = new JToolBar();
+        fileState = new JLabel("文件状态：未保存");
+        fileAttribute = new JLabel("长度：0    行数：1");
+        init();
+    }
+    /**
+     * 初始化对象属性
+     * @Author lrh
+     * @Date 2020/1/19 23:06
+     * @Param []
+     * @Return void
+     */
+    private void init(){
         jTextArea.setFont(new Font(Constants.FONT_NAME, Font.PLAIN, Constants.FONT_SIZE));   // 设置字体
         jScrollPane.setRowHeaderView(new LineNumberHeaderView());
-        this.setLayout(new BorderLayout(1,1));  //布局管理，这里一定要设置，否则会不显示
+        this.setLayout(new BorderLayout(1,2));  //布局管理，这里一定要设置，否则会不显示
+        jToolBar.setFloatable(false);  //设置为不可移动
+        jToolBar.add(fileState);
+        jToolBar.addSeparator();
+        jToolBar.add(fileAttribute);
+        fileState.setFont(new Font(Constants.FONT_NAME,Font.PLAIN,Constants.FONT_SIZE));
+        fileAttribute.setFont(new Font(Constants.FONT_NAME,Font.PLAIN,Constants.FONT_SIZE));
         this.add(jScrollPane,BorderLayout.CENTER); //设置据中显示
+        this.add(jToolBar,BorderLayout.PAGE_END);  //设置显示在底部
     }
-
     /**
      * 获取JTextArea对象
      * @Author lrh
@@ -67,5 +103,29 @@ public class TabCard extends JPanel {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public JToolBar getjToolBar() {
+        return jToolBar;
+    }
+
+    public void setjToolBar(JToolBar jToolBar) {
+        this.jToolBar = jToolBar;
+    }
+
+    public JLabel getFileState() {
+        return fileState;
+    }
+
+    public void setFileState(JLabel fileState) {
+        this.fileState = fileState;
+    }
+
+    public JLabel getFileAttribute() {
+        return fileAttribute;
+    }
+
+    public void setFileAttribute(JLabel fileAttribute) {
+        this.fileAttribute = fileAttribute;
     }
 }
