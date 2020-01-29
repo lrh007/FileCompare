c9317fa72a0fcb46756758b682d85f254592eeda
梁瑞豪 <18733558325@163.com>
2020-01-29 10:23:40 +0800

    '优化代码'

--- src/main/java/frame/FrameMenu.java
+++ src/main/java/frame/FrameMenu.java
@@ -14,6 +14,23 @@ import java.awt.event.KeyEvent;
  * @Version 1.0
  */
 public class FrameMenu {
+    private static JMenuBar jMenuBar = new JMenuBar();
+    private static JMenu fileMenu = new JMenu("文件(F)");
+    private static JMenu editMenu = new JMenu("编辑(E)");
+    private static JMenu searchMenu = new JMenu("搜索(S)");
+    private static JMenu viewMenu = new JMenu("视图(V)");
+    private static JMenu helpMenu = new JMenu("帮助(H)");
+    private static JMenuItem newFile = new JMenuItem("新建(N)    ");
+    private static JMenuItem openFile = new JMenuItem("打开(O)   ");
+    private static JMenuItem saveFile = new JMenuItem("保存(S)   ");
+    private static JMenuItem closeTab = new JMenuItem("关闭(W)   ");
+    private static JMenuItem exitSystem = new JMenuItem("退出(Q) ");
+    private static JMenuItem cancelOption = new JMenuItem("撤销(Z)   ");
+    private static JMenuItem resetOption = new JMenuItem("恢复(Y)    ");
+    private static JMenuItem cutFile = new JMenuItem("剪切(X)    ");
+    private static JMenuItem copyFile = new JMenuItem("复制(C)   ");
+    private static JMenuItem pasteFile = new JMenuItem("粘贴(V)  ");
+    private static Font font = new Font("微软雅黑",Font.PLAIN,18);
 
     private FrameMenu() {
     }
@@ -26,13 +43,59 @@ public class FrameMenu {
      * @Return void
      */
     public static void addMenu(final JFrame jFrame){
-        JMenuBar jMenuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("文件(F)");
-        JMenu editMenu = new JMenu("编辑(E)");
-        JMenu searchMenu = new JMenu("搜索(S)");
-        JMenu viewMenu = new JMenu("视图(V)");
-        JMenu helpMenu = new JMenu("帮助(H)");
-        Font font = new Font("微软雅黑",Font.PLAIN,18);
+        addOneMenu(jFrame);
+        jFrame.setJMenuBar(jMenuBar);
+    }
+    /**
+     * 添加一级菜单
+     * @Author lrh
+     * @Date 2020/1/29 10:09
+     * @Param []
+     * @Return void
+     */
+    private static void addOneMenu(JFrame jFrame){
+        jMenuBar.add(fileMenu);
+        jMenuBar.add(editMenu);
+        jMenuBar.add(searchMenu);
+        jMenuBar.add(viewMenu);
+        jMenuBar.add(helpMenu);
+        /**添加二级菜单**/
+        addTwoMenu(jFrame);
+    }
+    /**
+     * 添加二级菜单
+     * @Author lrh
+     * @Date 2020/1/29 10:10
+     * @Param []
+     * @Return void
+     */
+    private static void addTwoMenu(JFrame jFrame){
+        /**添加文件按钮的二级菜单**/
+        fileMenu.add(newFile);
+        fileMenu.add(openFile);
+        fileMenu.add(saveFile);
+        fileMenu.add(closeTab);
+        fileMenu.addSeparator(); //添加分割线
+        fileMenu.add(exitSystem);
+        /**添加编辑按钮的二级菜单**/
+        editMenu.add(cancelOption);
+        editMenu.add(resetOption);
+        editMenu.addSeparator();
+        editMenu.add(cutFile);
+        editMenu.add(copyFile);
+        editMenu.add(pasteFile);
+        /**添加菜单的快捷键**/
+        addHotKey(jFrame);
+    }
+    /**
+     * 添加菜单的快捷键
+     * @Author lrh
+     * @Date 2020/1/29 10:13
+     * @Param []
+     * @Return void
+     */
+    private static void addHotKey(JFrame jFrame){
+        /**添加所有一级菜单的快捷键**/
         fileMenu.setFont(font);
         fileMenu.setMnemonic(KeyEvent.VK_F);
         editMenu.setFont(font);
@@ -43,21 +106,7 @@ public class FrameMenu {
         viewMenu.setMnemonic(KeyEvent.VK_V);
         helpMenu.setFont(font);
         helpMenu.setMnemonic(KeyEvent.VK_H);
-        jMenuBar.add(fileMenu);
-        jMenuBar.add(editMenu);
-        jMenuBar.add(searchMenu);
-        jMenuBar.add(viewMenu);
-        jMenuBar.add(helpMenu);
-        JMenuItem newFile = new JMenuItem("新建(N)    ");
-        JMenuItem openFile = new JMenuItem("打开(O)   ");
-        JMenuItem saveFile = new JMenuItem("保存(S)   ");
-        JMenuItem closeTab = new JMenuItem("关闭(W)   ");
-        JMenuItem exitSystem = new JMenuItem("退出(Q) ");
-        JMenuItem cancelOption = new JMenuItem("撤销(Z)   ");
-        JMenuItem resetOption = new JMenuItem("恢复(Y)    ");
-        JMenuItem cutFile = new JMenuItem("剪切(X)    ");
-        JMenuItem copyFile = new JMenuItem("复制(C)   ");
-        JMenuItem pasteFile = new JMenuItem("粘贴(V)  ");
+        /**添加文件按钮下面所有子菜单的快捷键**/
         newFile.setFont(font);
         newFile.setMnemonic(KeyEvent.VK_N);
         newFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
@@ -73,14 +122,13 @@ public class FrameMenu {
         exitSystem.setFont(font);
         exitSystem.setMnemonic(KeyEvent.VK_Q);
         exitSystem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,ActionEvent.CTRL_MASK));
-
+        /**添加编辑按钮下面所有子菜单的快捷键**/
         cancelOption.setFont(font);
         cancelOption.setMnemonic(KeyEvent.VK_Z);
         cancelOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK));
         resetOption.setFont(font);
         resetOption.setMnemonic(KeyEvent.VK_Y);
         resetOption.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, ActionEvent.CTRL_MASK));
-
         copyFile.setFont(font);
         copyFile.setMnemonic(KeyEvent.VK_C);
         copyFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
@@ -90,33 +138,27 @@ public class FrameMenu {
         pasteFile.setFont(font);
         pasteFile.setMnemonic(KeyEvent.VK_V);
         pasteFile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK));
+        /**添加事件监听**/
+        addListener(jFrame);
+    }
 
-        fileMenu.add(newFile);
-        fileMenu.add(openFile);
-        fileMenu.add(saveFile);
-        fileMenu.add(closeTab);
-        fileMenu.addSeparator(); //添加分割线
-        fileMenu.add(exitSystem);
-
-        editMenu.add(cancelOption);
-        editMenu.add(resetOption);
-        editMenu.addSeparator();
-        editMenu.add(cutFile);
-        editMenu.add(copyFile);
-        editMenu.add(pasteFile);
-        jFrame.setJMenuBar(jMenuBar);
-
+    /**
+     * 添加事件监听
+     * @Author lrh
+     * @Date 2020/1/29 10:07
+     * @Param []
+     * @Return void
+     */
+    private static void addListener(JFrame jFrame){
         ComponentListener.exitSystemListener(jFrame,exitSystem);  //退出系统
         ComponentListener.openFileListener(jFrame,openFile);      //打开文件
         ComponentListener.newFileListener(jFrame,newFile);        //新建文件
         ComponentListener.closeTabListener(jFrame,closeTab);      //关闭当前选项卡
         ComponentListener.saveFileListener(jFrame,saveFile);      //保存文件
-
         ComponentListener.cancelOptionListener(cancelOption);     //撤销
         ComponentListener.resetOptionListener(resetOption);       //恢复
         ComponentListener.copyFileListener(copyFile);             //复制
         ComponentListener.cutFileListener(cutFile);               //剪切
         ComponentListener.pasteFileListener(pasteFile);           //粘贴
-
     }
 }
