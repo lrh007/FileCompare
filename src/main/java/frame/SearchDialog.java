3ec86a36d1cce84d61fe297a58e064f033f46139
梁瑞豪 <18733558325@163.com>
2020-03-15 21:27:25 +0800

    '添加替换全部功能'

--- src/main/java/frame/SearchDialog.java
+++ src/main/java/frame/SearchDialog.java
@@ -38,8 +38,20 @@ public class SearchDialog extends JDialog {
     private static JLabel tips = new JLabel();
     /**计数**/
     private JButton searchCount = new JButton("计数");
+    /**替换文本**/
+    private JLabel replaceTarget = new JLabel("替换为： ");
+    /**替换的字符串**/
+    private JTextField replaceStr = new JTextField();
+    /**替换按钮**/
+    private JButton replaceBtn = new JButton("替换");
+    /**替换全部按钮**/
+    private JButton replaceAllBtn = new JButton("替换全部");
     /**目标文本索引位置**/
     private int indexOf = 0;
+    /**文本替换标识**/
+    private boolean replaceFlag = false;
+    /**下一个文本替换标识**/
+    private boolean nextReplaceFlag = false;
     private SearchDialog(){
 
     }
@@ -54,6 +66,10 @@ public class SearchDialog extends JDialog {
         conn.add(searchNext);
         conn.add(tips);
         conn.add(searchCount);
+        conn.add(replaceTarget);
+        conn.add(replaceStr);
+        conn.add(replaceBtn);
+        conn.add(replaceAllBtn);
 
         conn.setBackground(Color.white);
         this.setSize(680,400);
@@ -84,6 +100,10 @@ public class SearchDialog extends JDialog {
         searchNext.setFont(Constants.FONT);
         searchTarget.setFont(Constants.FONT);
         searchCount.setFont(Constants.FONT);
+        replaceTarget.setFont(Constants.FONT);
+        replaceStr.setFont(Constants.FONT);
+        replaceBtn.setFont(Constants.FONT);
+        replaceAllBtn.setFont(Constants.FONT);
         tips.setFont(Constants.FONT);
         tips.setOpaque(true); //设置背景不是透明的，否则设置背景颜色不起作用
         tips.setBorder(new MatteBorder(1,0,0,0,Color.decode("#e6e4e0")));
@@ -94,11 +114,18 @@ public class SearchDialog extends JDialog {
         inputStr.setBounds(100,10,400,30);
         searchUp.setBounds(510,10,70,30);
         searchNext.setBounds(590,10,70,30);
+        replaceTarget.setBounds(10,60,90,30);
+        replaceStr.setBounds(100,60,400,30);
+        replaceBtn.setBounds(510,60,150,30);
+        replaceAllBtn.setBounds(510,110,150,30);
+        searchCount.setBounds(510,160,150,30);
         tips.setBounds(0,335,680,30);
-        searchCount.setBounds(510,60,150,30);
+
         upActionListener();
         nextActionListener();
         searchCountListener();
+        replaceListener();
+        replaceAllListener();
     }
     /**
      * 设置红色的错误提示信息
@@ -136,6 +163,11 @@ public class SearchDialog extends JDialog {
             public void actionPerformed(ActionEvent e) {
                 tips.setText(""); //先清空原来的文本
                 String searchStr = inputStr.getText();
+                if(searchStr.length() == 0){
+                    setErrorTipsMsg("查找： 请输入要查找的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
                 TabCard  tabCard = (TabCard)jTabbedPane.getSelectedComponent();
                 JTextArea jTextArea = tabCard.getjTextArea();
                 jTextArea.setSelectionColor(Color.green);
@@ -159,7 +191,7 @@ public class SearchDialog extends JDialog {
                         indexOf = jTextArea.getText().length();   //找不到时从尾部开始查找
                         jTextArea.setSelectionStart(0); //设置文本开始选中的位置
                         jTextArea.setSelectionEnd(0);
-                        setErrorTipsMsg("查找：无法找到文本\""+searchStr+"\"");
+                        setErrorTipsMsg("查找： 无法找到文本\""+searchStr+"\"");
                         FrameUtils.windowJitter(getInstance(),true);
                     }
                 }
@@ -182,6 +214,11 @@ public class SearchDialog extends JDialog {
             public void actionPerformed(ActionEvent e) {
                 tips.setText(""); //先清空原来的文本
                 String searchStr = inputStr.getText();
+                if(searchStr.length() == 0){
+                    setErrorTipsMsg("查找： 请输入要查找的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
                 TabCard  tabCard = (TabCard)jTabbedPane.getSelectedComponent();
                 JTextArea jTextArea = tabCard.getjTextArea();
                 jTextArea.setSelectionColor(Color.green);
@@ -204,7 +241,7 @@ public class SearchDialog extends JDialog {
                         indexOf = 0; //找不到时从头开始查找
                         jTextArea.setSelectionStart(0); //设置文本开始选中的位置
                         jTextArea.setSelectionEnd(0);
-                        setErrorTipsMsg("查找：无法找到文本\""+searchStr+"\"");
+                        setErrorTipsMsg("查找： 无法找到文本\""+searchStr+"\"");
                         FrameUtils.windowJitter(getInstance(),true);
                     }
                 }
@@ -243,6 +280,121 @@ public class SearchDialog extends JDialog {
             }
         });
     }
+    /**   
+     * 替换字符串
+     * @Author lrh
+     * @Date 2020/3/15 17:18
+     * @Param []
+     * @Return void
+     */
+    private void replaceListener(){
+        replaceBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tips.setText(""); //先清空原来的文本
+                String sourceStr = inputStr.getText(); //要查询的字符串
+                String targetStr = replaceStr.getText(); //要替换的字符串
+                if(sourceStr.length() == 0){
+                    setErrorTipsMsg("替换： 请输入要查找的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
+                if(targetStr.length() == 0){
+                    setErrorTipsMsg("替换： 请输入要替换的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
+                TabCard  tabCard = (TabCard)jTabbedPane.getSelectedComponent();
+                JTextArea jTextArea = tabCard.getjTextArea();
+                jTextArea.setSelectionColor(Color.green);
+                if(indexOf < 0){
+                    indexOf = 0;
+                }
+                //判断是否已经找到要替换的字符串
+                if(replaceFlag){
+                    jTextArea.replaceSelection(targetStr);
+                    indexOf = indexOf+targetStr.length();
+                }
+                int index = jTextArea.getText().indexOf(sourceStr,indexOf);
+                if(index >= 0 ){
+                    indexOf = index;
+                    jTextArea.setSelectionStart(index); //设置文本开始选中的位置
+                    jTextArea.setSelectionEnd(index+sourceStr.length()); //文本结束选中的位置
+                    replaceFlag = true;
+                    setNormalTipsMsg("替换： 已替换1个匹配项，并找到了下一个");
+                }else {  //找不到的情况下，再从文件开始位置找一下，防止点击下一个的时候中间出现一次不查询的情况
+                    indexOf = 0; //找不到时从头开始查找
+                    index = jTextArea.getText().indexOf(sourceStr,indexOf);
+                    if(index >=0 ){
+                        indexOf = index;
+                        jTextArea.setSelectionStart(index); //设置文本开始选中的位置
+                        jTextArea.setSelectionEnd(index+sourceStr.length()); //文本结束选中的位置
+                        replaceFlag = true;
+                        setNormalTipsMsg("替换： 已替换1个匹配项，并找到了下一个");
+                    }else{
+                        indexOf = 0; //找不到时从头开始查找
+                        jTextArea.setSelectionStart(0); //设置文本开始选中的位置
+                        jTextArea.setSelectionEnd(0);
+                        FrameUtils.windowJitter(getInstance(),true);
+                        replaceFlag = false;
+                        setErrorTipsMsg("替换： 没有找到匹配项\""+sourceStr+"\"");
+                    }
+                }
+            }
+        });
+    }
+    /**   
+     * 替换全部事件监听
+     * @Author lrh
+     * @Date 2020/3/15 20:37
+     * @Param []
+     * @Return void
+     */
+    private void replaceAllListener(){
+        replaceAllBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tips.setText(""); //先清空原来的文本
+                final String matchStr = inputStr.getText(); //查找的文本
+                final String targetStr = replaceStr.getText(); //要替换的文本
+                if(matchStr.length() == 0){
+                    setErrorTipsMsg("全部替换： 请输入要查找的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
+                if(targetStr.length() == 0){
+                    setErrorTipsMsg("全部替换： 请输入要替换的字符串");
+                    FrameUtils.windowJitter(getInstance(),true);
+                    return;
+                }
+                TabCard  tabCard = (TabCard)jTabbedPane.getSelectedComponent();
+                final JTextArea jTextArea = tabCard.getjTextArea();
+                jTextArea.setSelectionColor(Color.green);
+                final String sourceStr = jTextArea.getText();
+                new Thread(new Runnable() {
+                    public void run() {
+                        int count = matchCount(sourceStr,matchStr);
+                        if(count == 0){
+                            setErrorTipsMsg("全部替换： 已替换0个匹配项");
+                            FrameUtils.windowJitter(getInstance(),true);
+                        }else{
+                            int index = 0;
+                            while(true){
+                                int idx = jTextArea.getText().indexOf(matchStr,index);
+                                if(idx == -1){
+                                    break;
+                                }
+                                jTextArea.replaceRange(targetStr,index,index + matchStr.length());
+                                index = index + targetStr.length();
+                            }
+                            setNormalTipsMsg("全部替换： 已替换"+count+"个匹配项");
+                        }
+                        inputStr.requestFocus(); //输入框获取焦点
+                        inputStr.selectAll();
+                    }
+                }).start();
+            }
+        });
+    }
+    
     /**   
      * 查询文本匹配次数
      * @Author lrh
